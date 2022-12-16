2016dca1c3582656e751da75101d199d7994ed1a
MarkSama <vargamark555@gmail.com>
2022-12-16 08:44:13 +0100

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 
+#from django.db import models
 
 # Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from django.shortcuts import render
 
 
 class Post(models.Model):
